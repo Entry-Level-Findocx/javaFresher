a55fec966858c83176e7a0a104c9b114d4309dc8
Entry-Level-Findocx <ejcabral1616@gmail.com>
2023-08-01 16:10:10 +0800

    second commit

--- src/main/java/dev/cabral/content/calendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/cabral/content/calendar/repository/ContentCollectionRepository.java
@@ -1,11 +1,17 @@
 package dev.cabral.content.calendar.repository;
 
 import dev.cabral.content.calendar.model.Content;
+import dev.cabral.content.calendar.model.Status;
+import dev.cabral.content.calendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+
 
 @Repository
 public class ContentCollectionRepository {
@@ -20,5 +26,19 @@ public class ContentCollectionRepository {
 
     public Optional<Content> findById(Integer id){
         return content.stream().filter(c->c.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init(){
+        Content c = new Content(1,
+                "My first arcade",
+                "nandemonae",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        content.add(c);
     }
 }
